9996944688308367f95b2c79d487b63e59f5483f
Rafhael Monteiro <63571337+rafhaelmonteiro@users.noreply.github.com>
2021-11-19 16:54:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 //const url = "mongodb+srv://rafhaelOcean:<Rl6Y24i5fodocHdJ>@cluster0.tkaft.mongodb.net";
-const url = "mongodb://rafhaelOcean:<Rl6Y24i5fodocHdJ>@cluster0-shard-00-00.tkaft.mongodb.net:27017,cluster0-shard-00-01.tkaft.mongodb.net:27017,cluster0-shard-00-02.tkaft.mongodb.net"
+const url = "mongodb://rafhaelOcean:<Rl6Y24i5fodocHdJ>@cluster0-shard-00-00.tkaft.mongodb.net:27017,cluster0-shard-00-01.tkaft.mongodb.net:27017,cluster0-shard-00-02.tkaft.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-910hak-shard-0&authSource=admin&retryWrites=true&w=majority"
 const dbName = "oceanDB";
 
 async function main() {
